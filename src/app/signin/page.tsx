758bbfee2c4256c3dd8330151ed0a87bbe7b5cef
Andrew Aliaj <andyaliaj@gmail.com>
2025-01-31 17:01:45 -0500

    updates

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,15 +1,13 @@
-
-import { signIn } from "@/lib/auth"
- 
-export default function SignIn() {
+import GithubSignIn from "@/components/GithubSignIn"
+import GoogleSignIn from "@/components/GoogleSignIn"
+ export default function SignInPage() {
   return (
-    <form
-      action={async () => {
-        "use server"
-        await signIn("github")
-      }}
-    >
-      <button type="submit">Signin with GitHub</button>
-    </form>
+    <div>
+      <h1>Sign In</h1>
+      <div>
+        <GoogleSignIn />
+        <GithubSignIn />
+      </div>
+    </div>
   )
-} 
+}
