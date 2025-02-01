bbbeade284114baf2ccdb160ca0a5ef18ff2d839
Andrew Aliaj <andyaliaj@gmail.com>
2025-02-01 09:00:53 -0500

    updates

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,30 @@
 import NextAuth from "next-auth"
 import authConfig from "./auth.config"
- 
 import { PrismaClient } from "@prisma/client"
 import { PrismaAdapter } from "@auth/prisma-adapter"
- 
+
 const prisma = new PrismaClient()
- 
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
-  pages:{ signIn:"/signin" },
-  debug:true,
+  pages: { signIn: "/signin" },
+  debug: true,
+  callbacks: {
+    authorized({ request, auth }) {
+      const { pathname } = request.nextUrl
+      if (pathname.startsWith("/protected")) return !!auth
+      return true
+    },
+    async session({ session }) {  // Removed unused token parameter
+      return session
+    },
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    }
+  },
   ...authConfig,
 })
